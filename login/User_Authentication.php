677ac105aa3034f09623adec0fd3da0fd973ee23
sathyanaraayanaa <81378968+sathyanaraayanaa@users.noreply.github.com>
2021-06-28 12:22:12 +0530

    Add files via upload

---
+++ login/User_Authentication.php
@@ -0,0 +1,32 @@
+<?php
+    $server = "localhost";
+    $user = "root";
+    $pass = "";
+    $db = "catchcini";
+
+    $password = $_POST["password"];
+    $login = $_POST["login"];
+
+    $con = mysqli_connect($server, $user, $pass, $db);
+    $cmd = "SELECT username,mail,phone_no,password FROM user_details";
+    $data = mysqli_query($con, $cmd);
+    $flag = 0;
+    if($data) {
+        while($row = mysqli_fetch_assoc($data)){
+            if($row["username"]==$login || $row["mail"]==$login || $row["phone_no"]==$login){
+                if($row["password"]==$password){
+                    $flag = 1;
+                    break;
+                }
+            }  
+        } if($flag) {
+            echo '<script> alert("Welcome Back :)"); </script>';
+            echo '<script> window.location.href="../Homepage/home.html"; </script>';
+        } else {
+            echo '<script> alert("Incorrect Username or Password.\nPlease try again."); window.location.href="User_Login.php"; </script>';
+        }
+    } else {
+        echo $con->error;
+    }
+?>
+
