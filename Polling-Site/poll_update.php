a4ee66c4dc447bf02cbd80db8cbc7e80ab82dbf1
sh4ded <threalsk@gmail.com>
2021-07-01 22:56:20 +0530

    Add ref check retrieval

--- Polling-Site/poll_update.php
+++ Polling-Site/poll_update.php
@@ -25,25 +25,18 @@
       $flag=false;
     }
   }
-    $cmd = "SELECT ref,phone_no FROM $username";
+    $cmd = "SELECT phone_no FROM $username WHERE ref LIKE '%$ref%'";
     $data = mysqli_query($con, $cmd);
-    if($data) {
-        while($row = mysqli_fetch_assoc($data)){
-            if($row["ref"]==$ref){
-                $phone_no = $row["phone_no"];
-                break;
-            }
-        }
-        $flag = false;
-    }
+    $row = mysqli_fetch_assoc($data);
+    $phone_no = $row["phone_no"];
 
     $cmd = "INSERT INTO polls (username, phone_no, question, total_options, ref) VALUES ('$username', '$phone_no', '$question', '$hidden_val', '$ref')";
     $data = mysqli_query($con,$cmd);
     for($i =1; $i <= $hidden_val; $i++) {
         $opt = "option_$i";
-        $cmd = "UPDATE $username SET $opt = '$options[$i]' WHERE phone_no = $phone_no";
+        $cmd = "UPDATE $username SET $opt = '$options[$i]' WHERE ref LIKE '%$ref%'";
         $data = mysqli_query($con,$cmd);
-        $cmd = "UPDATE polls SET $opt = '$options[$i]' WHERE phone_no = $phone_no";
+        $cmd = "UPDATE polls SET $opt = '$options[$i]' WHERE ref LIKE '%$ref%'";
         $data = mysqli_query($con,$cmd);
     }
     echo '<script> window.location.href="User.php"; </script>';
