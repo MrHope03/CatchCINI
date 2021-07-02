40e0b58fa4f525a191c78baa604ff0aa1197ebf0
sh4ded <threalsk@gmail.com>
2021-07-02 14:00:43 +0530

    Add form ui/post behavior (partial)

--- Polling-Site/form_template.php
+++ Polling-Site/form_template.php
@@ -13,7 +13,7 @@ if (!$con) {
 //$username = $_SESSION["username"];
 //$ref = $_POST["ref"];
 $username = 'saikumarran';
-$ref = '2bpv8mxi';
+$ref = '2guj2llk';
 $cmd = "SELECT * FROM $username where ref LIKE '%$ref%'";
 $data = mysqli_query($con, $cmd);
 $row = mysqli_fetch_assoc($data);
@@ -82,16 +82,78 @@ for($i = 1; $i <= $tot_opts; $i++)
     <div>
       <h1 id="question"><?php echo $ques; ?></h1>
     </div>
+  </header>
   <section>
-    <div id="options">
+    <form method="post" action="<?=$_SERVER['PHP_SELF'];?>">
+    <div id="options-box">
       <?php for($i = 1; $i <= $tot_opts; $i++)
-      echo '<div class="option_'.$i.'">'.$options[$i].'</div>';
+      echo '<div class="options" id="option_'.$i.'" onclick="get_vote(this.id)">'.$options[$i].'</div>';
       ?>
-    <div>
+    <div id="button-box">
+      <input id="button" name="button" type="button" value="Vote">
+      <input type="hidden" name="hidden-value" id="hidden-value">
+    </div>
+  </form>
     </section>
 
     <script>
+    function isOverflown(element) {
+      return element.scrollHeight > element.clientHeight || element.scrollWidth > element.clientWidth;
+    }
+    var ques = document.getElementById('question');
+    if (!isOverflown(ques))
+    {
+      ques.style.display = "flex";
+      ques.style.alignItems = "center";
+      ques.style.justifyContent = "center";
+    }
+
+    var first_click = false;
+    var sel_opt = NULL;
+    var butt = NULL;
+
+    function get_vote(opt_id){
+      if (!first_click){
+      sel_opt = document.getElementById(opt_id);
+      document.getElementById('hidden-value').value = sel_opt.id;
+      sel_opt.style.borderColor = 'rgb(2, 84, 244)';
+      first_click = true;
+      butt = document.getElementById('button');
+      butt.type = 'submit';
+      butt.style.color = 'black';
+      butt.style.opacity = '100%';
+      butt.style.backgroundColor = 'rgb(245, 245, 245)';
+      butt.style.border = 'grey 0.5px solid';
+      butt.classList.add('button-style-hover');
+      butt.classList.add('button-style-active');
+      }
+      else{
+        sel_opt.style.borderColor = 'rgb(11, 174, 185)';
+        if (sel_opt.id != opt_id){
+        sel_opt = document.getElementById(opt_id);
+        document.getElementById('hidden-value').value = sel_opt.id;
+        sel_opt.style.borderColor = 'rgb(2, 84, 244)';
+        }
+        else{
+          butt.style.color = 'grey';
+          butt.style.opacity = '50%';
+          butt.style.backgroundColor = 'darkgrey';
+          butt.style.border = '0px';
+          butt.classList.remove('button-style-hover');
+          butt.classList.remove('button-style-active');
+          butt.type = 'button';
+          first_click = false;
+        }
+      }
+    }
 
     </script>
+    <?php
+    if(isset($_POST['button'])){
+      $upd_id = $_POST['hidden-value'];
+      echo "<script>alert('Click to view results!');</script>";
+    }
+    ?>
+
   </body>
 </html>
