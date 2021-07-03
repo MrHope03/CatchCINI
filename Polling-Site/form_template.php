614478580dcb950a2fd3f30142de88e83b39f59b
sh4ded <threalsk@gmail.com>
2021-07-03 08:47:57 +0530

    Add popcorn (css to-do)

--- Polling-Site/form_template.php
+++ Polling-Site/form_template.php
@@ -8,13 +8,15 @@ $con = mysqli_connect($server, $user, $pass, $db);
 if (!$con) {
     echo '<script> alert("Server Down!!! Try again Later"); </script>';
 }
-
 //remove below comments after finishing dependent checks
 //$username = $_SESSION["username"];
 //$ref = $_POST["ref"];
-$username = 'saikumarran';
-$ref = '2guj2llk';
-$cmd = "SELECT * FROM $username where ref LIKE '%$ref%'";
+$ref = '1w9ww5ko';
+$cmd = "SELECT username FROM polls where ref LIKE '%$ref%'";
+$data = mysqli_query($con, $cmd);
+$row = mysqli_fetch_assoc($data);
+$username = $row["username"];
+$cmd = "SELECT * from $username where ref LIKE '%$ref%'";
 $data = mysqli_query($con, $cmd);
 $row = mysqli_fetch_assoc($data);
 $fname = $row["first_name"];
@@ -87,11 +89,105 @@ for($i = 1; $i <= $tot_opts; $i++)
     <form method="post" action="<?=$_SERVER['PHP_SELF'];?>">
     <div id="options-box">
       <?php for($i = 1; $i <= $tot_opts; $i++)
-      echo '<div class="options" id="option_'.$i.'" onclick="get_vote(this.id)">'.$options[$i].'</div>';
+      echo '<div class="options" id="'.$i.'" onclick="get_vote(this.id)">'.$options[$i].'</div>';
       ?>
     <div id="button-box">
       <input id="button" name="button" type="button" value="Vote">
       <input type="hidden" name="hidden-value" id="hidden-value">
+      <input type="hidden" name="check" value="true">
+    </div>
+    </div>
+    <div id="graph">
+        <div id="pop_1" class="popcorn">
+            <img src="popcorn.png">
+            <div class="container">
+                <!-- RED -->
+                <div class="red box"></div>
+                <!-- WHITE -->
+                <div class="white box"></div>
+                <!-- RED -->
+                <div class="red box"></div>
+                <!-- WHITE -->
+                <div class="white box"></div>
+                <!-- RED -->
+                <div class="red box"></div>
+            </div>
+        </div>
+        <div id="pop_2" class="popcorn">
+            <img src="popcorn.png">
+            <div class="container">
+                <!-- RED -->
+                <div class="red box"></div>
+                <!-- WHITE -->
+                <div class="white box"></div>
+                <!-- RED -->
+                <div class="red box"></div>
+                <!-- WHITE -->
+                <div class="white box"></div>
+                <!-- RED -->
+                <div class="red box"></div>
+            </div>
+        </div>
+        <div id="pop_3" class="popcorn">
+            <img src="popcorn.png">
+            <div class="container">
+                <!-- RED -->
+                <div class="red box"></div>
+                <!-- WHITE -->
+                <div class="white box"></div>
+                <!-- RED -->
+                <div class="red box"></div>
+                <!-- WHITE -->
+                <div class="white box"></div>
+                <!-- RED -->
+                <div class="red box"></div>
+            </div>
+        </div>
+        <div id="pop_4" class="popcorn">
+            <img src="popcorn.png">
+            <div class="container">
+                <!-- RED -->
+                <div class="red box"></div>
+                <!-- WHITE -->
+                <div class="white box"></div>
+                <!-- RED -->
+                <div class="red box"></div>
+                <!-- WHITE -->
+                <div class="white box"></div>
+                <!-- RED -->
+                <div class="red box"></div>
+            </div>
+        </div>
+        <div id="pop_5" class="popcorn">
+            <img src="popcorn.png">
+            <div class="container">
+                <!-- RED -->
+                <div class="red box"></div>
+                <!-- WHITE -->
+                <div class="white box"></div>
+                <!-- RED -->
+                <div class="red box"></div>
+                <!-- WHITE -->
+                <div class="white box"></div>
+                <!-- RED -->
+                <div class="red box"></div>
+            </div>
+        </div>
+        <div id="pop_6" class="popcorn">
+            <img src="popcorn.png">
+            <div class="container">
+                <!-- RED -->
+                <div class="red box"></div>
+                <!-- WHITE -->
+                <div class="white box"></div>
+                <!-- RED -->
+                <div class="red box"></div>
+                <!-- WHITE -->
+                <div class="white box"></div>
+                <!-- RED -->
+                <div class="red box"></div>
+            </div>
+        </div>
     </div>
   </form>
     </section>
@@ -111,11 +207,12 @@ for($i = 1; $i <= $tot_opts; $i++)
     var first_click = false;
     var sel_opt = NULL;
     var butt = NULL;
+    var post_first_click = false;
 
     function get_vote(opt_id){
       if (!first_click){
       sel_opt = document.getElementById(opt_id);
-      document.getElementById('hidden-value').value = sel_opt.id;
+      document.getElementById('hidden-value').value = "count_" + sel_opt.id;
       sel_opt.style.borderColor = 'rgb(2, 84, 244)';
       first_click = true;
       butt = document.getElementById('button');
@@ -127,11 +224,11 @@ for($i = 1; $i <= $tot_opts; $i++)
       butt.classList.add('button-style-hover');
       butt.classList.add('button-style-active');
       }
-      else{
+      else if(!post_first_click){
         sel_opt.style.borderColor = 'rgb(11, 174, 185)';
         if (sel_opt.id != opt_id){
         sel_opt = document.getElementById(opt_id);
-        document.getElementById('hidden-value').value = sel_opt.id;
+        document.getElementById('hidden-value').value = "count_" + sel_opt.id;
         sel_opt.style.borderColor = 'rgb(2, 84, 244)';
         }
         else{
@@ -151,9 +248,41 @@ for($i = 1; $i <= $tot_opts; $i++)
     <?php
     if(isset($_POST['button'])){
       $upd_id = $_POST['hidden-value'];
+      $cmd = "UPDATE $username SET $upd_id = IFNULL($upd_id,0) + 1 WHERE ref LIKE '%$ref%'";
+      $data = mysqli_query($con,$cmd);
+      $cmd = "UPDATE polls SET $upd_id = IFNULL($upd_id,0) + 1 WHERE ref LIKE '%$ref%'";
+      $data = mysqli_query($con,$cmd);
+      $cmd = "UPDATE $username SET total_count = IFNULL(total_count,0) + 1 WHERE ref LIKE '%$ref%'";
+      $data = mysqli_query($con,$cmd);
+      $cmd = "UPDATE polls SET total_count = IFNULL(total_count,0) + 1 WHERE ref LIKE '%$ref%'";
+      $data = mysqli_query($con,$cmd);
       echo "<script>alert('Click to view results!');</script>";
     }
+    if (isset($_POST["check"])){
+      $cmd = "SELECT * from $username where ref LIKE '%$ref%'";
+      $data = mysqli_query($con, $cmd);
+      $row = mysqli_fetch_assoc($data);
+      echo "<script>";
+      $sel_id = substr($upd_id,6);
+      echo "alert('$sel_id');";
+      echo "sel_opt = document.getElementById('$sel_id');";
+      echo "sel_opt.style.borderColor = 'rgb(2, 84, 244)';";
+      echo "first_click = true;";
+      echo "post_first_click = true;";
+      echo "document.getElementById('graph').style.display = 'flex';";
+      echo "var graph_data = [];";
+      for($i = 1; $i <= $tot_opts; $i++){
+        //echo "document.getElementById('pop_$i').style.visibility = 'visible';";
+        $temp = ($row['count_'.$i]/$row['total_count'])*200;
+        echo "graph_data[$i] = $temp;";
+        echo "alert(graph_data[$i]);";
+      }
+      echo "var total = $tot_opts;";
+      echo "for(var i = 1; i <= total; i++)";
+      echo "{document.getElementById('pop_'+i).style.visibility = 'visible';";
+      echo "}";
+      echo "</script>";
+    }
     ?>
-
   </body>
 </html>
